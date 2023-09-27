b171a826d9df17ca0242e27757868d054a811e56
Yun Xi Chen <cadeychen@MacBook-Pro.local>
2023-09-28 01:26:14 +0200

    28.09.2023: Added MUI dependencies and button

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import './Home.css';
 import c2clogo from '../images/C2C_2024_Logo.png';
+import Button from '@mui/material/Button';
 
 function Home() {
     return (
         <div className="home">
             <div className="image-wrapper">
                 <img src={c2clogo} alt="official logo of C2C" height={600} width={600}/>
+                <div className="buton-wrapper">
+                    <Button variant="outlined" sx={{
+                        color: 'white',
+                        ml: '30%',
+                        borderColor: 'white',
+                    }}>Learn more</Button>
+                </div>
             </div>
         </div>
     )
